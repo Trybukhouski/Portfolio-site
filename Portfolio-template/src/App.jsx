6765e4fef9aed5587e9363b1b9d3ab6e4da42b91
Trybukhouski <trybukhouskij@gmail.com>
2025-07-28 20:21:01 +0300

    feat: create a Footer component. Closes #5

--- Portfolio-template/src/App.jsx
+++ Portfolio-template/src/App.jsx
@@ -1,11 +1,7 @@
 import "./styles/main.css"
-import vk from "./img/icons/vk.svg"
-import twitter from "./img/icons/twitter.svg"
-import linkedIn from "./img/icons/linkedIn.svg"
-import instagram from "./img/icons/instagram.svg"
-import gitHub from "./img/icons/gitHub.svg"
 import NavBar from "./components/navbar/NavBar"
 import Header from "./components/header/Header"
+import Footer from "./components/footer/Footer"
 
 
 
@@ -30,23 +26,7 @@ function App() {
 
           </div>
       </main>
-
-      <footer className="footer">
-          <div className="container">
-              <div className="footer__wrapper">
-                  <ul className="social">
-                      <li className="social__item"><a href="#!"><img src={vk} alt="Link" /></a></li>
-                      <li className="social__item"><a href="#!"><img src={instagram} alt="Link" /></a></li>
-                      <li className="social__item"><a href="#!"><img src={twitter} alt="Link" /></a></li>
-                      <li className="social__item"><a href="#!"><img src={gitHub} alt="Link" /></a></li>
-                      <li className="social__item"><a href="#!"><img src={linkedIn} alt="Link" /></a></li>
-                  </ul>
-                  <div className="copyright">
-                      <p>© 2022 frontend-dev.com</p>
-                  </div>
-              </div>
-          </div>
-      </footer>
+      <Footer />
     </>
   )
 }
