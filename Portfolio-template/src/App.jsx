a45c408fa272470ec6014ad0c79eda607e9222ef
Trybukhouski <trybukhouskij@gmail.com>
2025-07-28 19:51:38 +0300

    feat: create Header component. Closes #6

--- Portfolio-template/src/App.jsx
+++ Portfolio-template/src/App.jsx
@@ -5,6 +5,7 @@ import linkedIn from "./img/icons/linkedIn.svg"
 import instagram from "./img/icons/instagram.svg"
 import gitHub from "./img/icons/gitHub.svg"
 import NavBar from "./components/navbar/NavBar"
+import Header from "./components/header/Header"
 
 
 
@@ -12,19 +13,7 @@ function App() {
   return (
     <>
         <NavBar />
-      <header className="header">
-          <div className="header__wrapper">
-              <h1 className="header__title">
-                  <strong>Hi, my name is <em>Yuri</em></strong><br />
-                  a frontend developer
-              </h1>
-              <div className="header__text">
-                  <p>with passion for learning and creating.</p>
-              </div>
-              <a href="#!" className="btn">Download CV</a>
-          </div>
-      </header>
-
+        <Header />
       <main className="section">
           <div className="container">
 
