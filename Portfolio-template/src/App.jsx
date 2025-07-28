ef1f9c0926f9a233b2d3028393d8d8178908f3cb
Trybukhouski <trybukhouskij@gmail.com>
2025-07-28 19:28:58 +0300

    feat: create a navbar component. Closes #4

--- Portfolio-template/src/App.jsx
+++ Portfolio-template/src/App.jsx
@@ -1,36 +1,17 @@
 import "./styles/main.css"
-import sun from "./img/icons/sun.svg"
-import moon from "./img/icons/moon.svg"
 import vk from "./img/icons/vk.svg"
 import twitter from "./img/icons/twitter.svg"
 import linkedIn from "./img/icons/linkedIn.svg"
 import instagram from "./img/icons/instagram.svg"
 import gitHub from "./img/icons/gitHub.svg"
+import NavBar from "./components/navbar/NavBar"
 
 
 
 function App() {
   return (
     <>
-      <nav className="nav">
-        <div className="container">
-            <div className="nav-row">
-                <a href="./index.html" className="logo"><strong>Freelancer</strong> portfolio</a>
-
-                <button className="dark-mode-btn">
-                    <img src={sun} alt="Light mode" className="dark-mode-btn__icon"/>
-                    <img src={moon} alt="Dark mode" className="dark-mode-btn__icon"/>
-                </button>
-
-                <ul className="nav-list">
-                    <li className="nav-list__item"><a href="./index.html" className="nav-list__link nav-list__link--active">Home</a></li>
-                    <li className="nav-list__item"><a href="./projects.html" className="nav-list__link">Projects</a></li>
-                    <li className="nav-list__item"><a href="./contacts.html" className="nav-list__link">Contacts</a></li>
-                </ul>
-            </div>
-        </div>
-      </nav>
-
+        <NavBar />
       <header className="header">
           <div className="header__wrapper">
               <h1 className="header__title">
