83a7ea62cee8e467f89d10b523807fc5916e6c20
Trybukhouski <trybukhouskij@gmail.com>
2025-07-30 17:35:08 +0300

    feat: create ContactsPage component. Closes #12

--- Portfolio-template/src/App.jsx
+++ Portfolio-template/src/App.jsx
@@ -1,14 +1,15 @@
 import "./styles/main.css";
 import NavBar from "./components/navbar/NavBar";
 import Footer from "./components/footer/Footer";
-import ProjectsPage from "./pages/projectspage/ProjectsPage";
+// import ProjectsPage from "./pages/projectspage/ProjectsPage";
+import ContactsPage from "./pages/contactspage/ContactsPAge";
 // import Homepage from "./pages/homepage/Homepage";
 
 function App() {
   return (
     <>
       <NavBar />
-      <ProjectsPage />
+      <ContactsPage />
       <Footer />
     </>
   );
