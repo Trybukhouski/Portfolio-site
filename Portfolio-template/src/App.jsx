6ad80d6e6468768def4b9d4de50a2f5ec48f95ac
Trybukhouski <trybukhouskij@gmail.com>
2025-07-30 18:57:01 +0300

    fix: add styles to contacts page

--- Portfolio-template/src/App.jsx
+++ Portfolio-template/src/App.jsx
@@ -1,16 +1,16 @@
 import "./styles/main.css";
 import NavBar from "./components/navbar/NavBar";
 import Footer from "./components/footer/Footer";
-import ProjectsPage from "./pages/projectspage/ProjectsPage";
-import SingleProjectPage from "./pages/SingleProjectPage/SingleProjectPage";
+// import ProjectsPage from "./pages/projectspage/ProjectsPage";
+// import SingleProjectPage from "./pages/SingleProjectPage/SingleProjectPage";
 // import ContactsPage from "./pages/contactspage/ContactsPAge";
-// import Homepage from "./pages/homepage/Homepage";
+import Homepage from "./pages/homepage/Homepage";
 
 function App() {
   return (
     <>
       <NavBar />
-      <SingleProjectPage />
+      <Homepage />
       <Footer />
     </>
   );
