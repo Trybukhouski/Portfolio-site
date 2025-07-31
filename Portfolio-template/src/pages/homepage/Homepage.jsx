b2986e0186301573ab471b4a6604db1d13c8c6be
Trybukhouski <trybukhouskij@gmail.com>
2025-07-31 13:06:18 +0300

    chore: connect tailwind with project. Closes #14

--- Portfolio-template/src/pages/homepage/Homepage.jsx
+++ Portfolio-template/src/pages/homepage/Homepage.jsx
@@ -12,7 +12,7 @@ const Homepage = () => {
           <ul className="content-list">
             <li className="content-list__item">
               <h2 className="title-2">Frontend</h2>
-              <p>
+              <p className="font-bold underline">
                 JavaScript, TypeScript, ReactJS, Angular, Redux, HTML, CSS, NPM,
                 BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents
               </p>
